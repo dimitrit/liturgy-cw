616c18e4de18347f797185dd7d238bb86ca39c44
dimitrit <dimitri@tastypies.co.uk>
2025-05-11 16:01:19 +0100

    Add build file

---
+++ build.lua
@@ -0,0 +1,22 @@
+-- Build configuration for scripture
+
+module = "liturgy-cw"
+checkformat = "latex"
+stdengine    = "xelatex"
+checkengines = {"xelatex"}
+checkconfigs = {"build", "config-unicode"}
+typesetexe = "lualatex"
+
+-- Detail how to set the version automatically
+function update_tag(file, content, tagname, tagdate)
+if string.match (file, "%.dtx$") then
+local tagdate = string.gsub(tagdate, "%-", "/")
+content = string.gsub(content,
+"{%d%d%d%d/%d%d/%d%d v%d%.%d+}\n",
+"{" .. tagdate .. " v" .. tagname .. "}\n")
+content = string.gsub(content,
+"{%d%d%d%d/%d%d/%d%d}{%d%.%d+}\n",
+"{" .. tagdate .. "}{" .. tagname .. "}\n")
+end
+return content
+end
